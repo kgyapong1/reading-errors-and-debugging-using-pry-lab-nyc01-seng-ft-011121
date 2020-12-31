93a6a0e2fad7f255b201319f57b75a6ee783368b
Kwaku Gyapong <kwakugyapong219@gmail.com>
2020-12-31 00:35:09 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -9,7 +9,20 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
+def crew
+
+["Hello Geordi.", "Hello Data.", "Hello Worf.", "Hello William.", "Hello Beverly.", "Hello Deanna."]
+
+def greet_crew (crew)
+
+  crew.each {|crew_member| "Hello #{crew_member}."}
+end
+
+
 def engage
-  puts state_log(date)
   date = generate_star_date
+  
+  state_log(date)
+  greet_crew(crew)
+
 end
