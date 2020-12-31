0f594cc071aa6b1c33a72f8ccd5c2124027c1102
Kwaku Gyapong <kwakugyapong219@gmail.com>
2020-12-31 00:36:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,12 +1,10 @@
 
 
-def start_game(player1 = "Rocky" , player2 = "Bullwinkle" )
- return "Hello #{player1} & #{player2}"
+def start_game(player1, player2)
+  "Hello #{player1} & #{player2}"
 end
-
 
 def play_game(player1, player2)
   "#{player1} is better than #{player2}"
 end
-
 
